2ad1c3365cfb62f88ff6c514f33ac1ca31bcc722
swetachaurasiya85-byte <swetachaurasiya85@gmail.com>
2025-11-03 11:05:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7d1540f5-30c5-4c0f-a627-62a8483a5129",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
